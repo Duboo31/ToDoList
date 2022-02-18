10b33fac3622e658b44087ae89a77893c6cfa88d
duboo <duboo31@gmail.com>
2022-02-19 03:18:06 +0900

    ADD .gitignore & weather.js

---
+++ js/weather.js
@@ -0,0 +1,17 @@
+const onGeoOk = (position) => {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  fetch(url).then(response => response.json()).then(data => {
+    const weather = document.querySelector('#weather span:first-child');
+    const city = document.querySelector('#weather span:last-child');
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+  })
+}
+
+const onGeoError = (err) => {
+  console.log(`Error ${err}`)
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
