63c41e3a59d5f2fe30a11aec7dfaa11339c47b68
duboo <duboo31@gmail.com>
2022-02-18 19:59:16 +0900

    ADD toDo.js

---
+++ js/todo.js
@@ -0,0 +1,54 @@
+const toDoForm = document.querySelector('form#todo-form');
+const toDoInput = toDoForm.querySelector('input');
+const toDoList = document.querySelector('ul#todo-list');
+
+const TODOS_KEY = 'todos'
+
+let toDos = [];
+
+const saveToDos = () => {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+}
+
+const deleteTodo = (event) => {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+
+const paintToDo = (newTodo) => {
+  const li = document.createElement('li');
+  li.id = newTodo.id;
+  const span = document.createElement('span');
+  span.innerText = newTodo.text;
+  const button = document.createElement('button');
+  button.innerText = "X";
+  button.addEventListener('click', deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);  
+}
+
+const handleTodoSubmit = (event) => {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newToDoObj = {
+    text: newTodo,
+    id: Date.now()
+  }
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleTodoSubmit); 
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
